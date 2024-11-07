cc344134069eb72b51aa07d08d8e34c47286334c
Wei-Cheng Chao <weicheng9722@gmail.com>
2024-11-06 22:51:10 -1000

    create user model

--- counseling/db/schema.rb
+++ counseling/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_26_004736) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_07_013035) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -31,17 +31,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_26_004736) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "emotional_analyses", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "session_id", null: false
-    t.string "emotion", null: false
-    t.decimal "confidence_score", precision: 5, scale: 2, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["session_id"], name: "index_emotional_analyses_on_session_id"
-    t.index ["user_id"], name: "index_emotional_analyses_on_user_id"
-  end
-
   create_table "sessions", force: :cascade do |t|
     t.string "title"
     t.datetime "session_date"
@@ -52,12 +41,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_26_004736) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_foreign_key "emotional_analyses", "sessions"
-  add_foreign_key "emotional_analyses", "users"
 end
