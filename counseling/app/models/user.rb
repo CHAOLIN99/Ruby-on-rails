bdb8d6530a58a911986071c19c1bb1daf4f4cf73
Wei-Cheng Chao <weicheng9722@gmail.com>
2024-11-07 22:30:36 -1000

    Add user signup

--- counseling/app/models/user.rb
+++ counseling/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, format: { with: /@/, message: "must contain @" }
 end
--- counseling/config/routes.rb
+++ counseling/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new"
+  resources :users
   resources :counselors do
     resources :sessions
   end
