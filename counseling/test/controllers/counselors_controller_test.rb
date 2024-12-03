030c96aae36e8b01183612059df6d7870a912b54
Wei-Cheng Chao <weicheng9722@gmail.com>
2024-12-03 13:42:01 -1000

    Delet test files

--- counseling/test/controllers/counselors_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CounselorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get counselors_index_url
-    assert_response :success
-  end
-end
