dd71052b1e5b0feee0fecfb5eb6d9c4ff8c99369
Wei-Cheng Chao <weicheng9722@gmail.com>
2024-12-03 14:22:43 -1000

    Add intergrate test

--- counseling/test/test_helper.rb
+++ counseling/test/test_helper.rb
@@ -2,14 +2,28 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+#Use Spec DSL
+require 'minitest/spec'
+
+#Helpers
+require 'capybara/rails'
+require 'mocha/minitest'
+
+Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new(color: true), ENV, Minitest.backtrace_filter) 
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    #fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    extend Minitest::Spec::DSL
   end
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
 end
